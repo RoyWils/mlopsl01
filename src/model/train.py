e4ad3e41e3b909e7a5088274bb3baf1d40da8086
RoyWils <roywils@microsoft.com>
2023-01-20 18:21:54 +0000

    update

--- src/model/train.py
+++ src/model/train.py
@@ -38,8 +38,8 @@ def split_data(df):
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
-    LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
-
+    mod = LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
+    return mod
 
 def parse_args():
     # setup arg parser
